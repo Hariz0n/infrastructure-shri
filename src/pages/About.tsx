85f97abda30172230e9acbb3c5f96e76d647a17f
Danila Petrov <dp200308@gmail.com>
2023-07-16 22:01:57 +0500

    feat: add about translation

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,7 +5,7 @@ export const About: FC = () => {
   return (
     <>
       <Helmet>
-        <title>About page</title>
+        <title>About page | О страница</title>
       </Helmet>
       <h1 data-testid="page-title">About</h1>
       <p>This is an example application.</p>
