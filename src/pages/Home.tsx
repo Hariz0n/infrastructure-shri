04fc3913ab2e74feedb059018bd261df65e69b34
Danila Petrov <dp200308@gmail.com>
2023-07-16 22:17:26 +0500

    feat: add russian translation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,7 @@ export const Home: FC = () => {
       <Helmet>
         <title>Home page | Домашняя страница</title>
       </Helmet>
-      <h1 data-testid="page-title">Home</h1>
+      <h1 data-testid="page-title">Home | Домашняя страница</h1>
       <p>This is the list.</p>
       <TodoList />
     </>
